eb9060d77705223831d37ae7573647fb5493a89f
Arsen Markotskyi <arsen.markotskui@gmail.com>
2025-04-18 23:04:34 +0300

    "sol"

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import get_db, MovieModel
+from src.database import get_db, MovieModel
 
-from schemas import MovieListResponseSchema, MovieDetailResponseSchema
+from src.schemas import MovieListResponseSchema, MovieDetailResponseSchema
 
 
 router = APIRouter()
